a8f0224233bf978a15c646a90a2ac74531156af6
LopStevn <stevenlpz03@gmail.com>
2023-08-21 09:41:30 -0600

    Avance 9:30 - Creando la DAL

--- SVLT20232108.AccesoADatos/src/svlt20232108/accesoadatos/BookDAL.java
+++ SVLT20232108.AccesoADatos/src/svlt20232108/accesoadatos/BookDAL.java
@@ -4,10 +4,62 @@
  */
 package svlt20232108.accesoadatos;
 
+import svlt20232108.entidadesdenegocio.Book;
+import java.sql.*;
+
 /**
  *
  * @author MINEDUCYT
  */
 public class BookDAL {
     
+    // Metodo para obtener los campos a utilizar en la consulta SELECT de la tabla de Rol
+    static String obtenerCampos() {
+        return "r.Id,r.Nombre,r.Autor,r.Año";
+    }
+    
+    // Metodo para obtener el SELECT a la tabla Rol y el TOP en el caso que se utilice una base de datos SQL SERVER
+    private static String obtenerSelect(Book pBook) {
+        String sql;
+        sql = "SELECT ";
+        if (pBook.getTop_aux() > 0 && ComunDB.TIPODB == ComunDB.TipoDB.SQLSERVER) {
+            // Agregar el TOP a la consulta SELECT si el gestor de base de datos es SQL SERVER y "getTop_aux" es mayor a cero
+            sql += "TOP " + pBook.getTop_aux() + " ";
+        }
+        sql += (obtenerCampos() + " FROM Books r"); // Agregar los campos de la tabla de Rol mas el FROM a la tabla Rol con su alias "r"
+        return sql;
+    }
+    
+    // Metodo para obtener Order by a la consulta SELECT de la tabla Rol y ordene los registros de mayor a menor 
+    private static String agregarOrderBy(Book pBook) {
+        String sql = " ORDER BY r.Id DESC";
+        if (pBook.getTop_aux() > 0 && ComunDB.TIPODB == ComunDB.TipoDB.MYSQL) {
+            // Agregar el LIMIT a la consulta SELECT de la tabla de Rol en el caso que getTop_aux() sea mayor a cero y el gestor de base de datos
+            // sea MYSQL
+            sql += " LIMIT " + pBook.getTop_aux() + " ";
+        }
+        return sql;
+    }
+    
+    // Metodo para poder insertar un nuevo registro en la tabla de Rol
+    public static int crear(Book pBook) throws Exception {
+        int result;
+        String sql;
+        try (Connection conn = ComunDB.obtenerConexion();) { // Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            sql = "INSERT INTO Tareas(Name,Details,Done) VALUES(?,?,?)"; // Definir la consulta INSERT a la tabla de Rol utilizando el simbolo ? para enviar parametros
+            try (PreparedStatement ps = ComunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                ps.setString(1, pBook.getTitulo()); // Agregar el parametro a la consulta donde estan el simbolo ? #1  
+                ps.setString(2, pBook.getAutor()); 
+  
+                result = ps.executeUpdate(); // Ejecutar la consulta INSERT en la base de datos
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex; // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda 
+            }
+            conn.close(); // Cerrar la conexion a la base de datos
+        } catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion en el caso que suceda 
+        }
+        return result; // Retornar el numero de fila afectadas en el INSERT en la base de datos 
+    }
 }
--- SVLT20232108.EntidadesDeNegocio/src/svlt20232108/entidadesdenegocio/Book.java
+++ SVLT20232108.EntidadesDeNegocio/src/svlt20232108/entidadesdenegocio/Book.java
@@ -6,4 +6,61 @@ package svlt20232108.entidadesdenegocio;
  */
 public class Book {
     
+    private int Id;
+    private String Titulo;
+    private String Autor;
+    private int AñoPubli;
+    private int top_aux;
+
+    public Book() {
+    }
+
+    public Book(int Id, String Titulo, String Autor, int AñoPubli) {
+        this.Id = Id;
+        this.Titulo = Titulo;
+        this.Autor = Autor;
+        this.AñoPubli = AñoPubli;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String Nombre) {
+        this.Titulo = Titulo;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String Autor) {
+        this.Autor = Autor;
+    }
+
+    public int getAñoPubli() {
+        return AñoPubli;
+    }
+
+    public void setAñoPubli(int AñoPubli) {
+        this.AñoPubli = AñoPubli;
+    }
+
+    public int getTop_aux() {
+        return top_aux;
+    }
+
+    public void setTop_aux(int top_aux) {
+        this.top_aux = top_aux;
+    }
+    
+    
 }
