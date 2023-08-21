321949e73c67c8aafe0011d48ee1e6d585d30583
LopStevn <stevenlpz03@gmail.com>
2023-08-21 09:57:39 -0600

    Avance 10:00 - DAL

--- SVLT20232108.AccesoADatos/src/svlt20232108/accesoadatos/BookDAL.java
+++ SVLT20232108.AccesoADatos/src/svlt20232108/accesoadatos/BookDAL.java
@@ -6,6 +6,7 @@ package svlt20232108.accesoadatos;
 
 import svlt20232108.entidadesdenegocio.Book;
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,7 +16,7 @@ public class BookDAL {
     
     // Metodo para obtener los campos a utilizar en la consulta SELECT de la tabla de Rol
     static String obtenerCampos() {
-        return "r.Id,r.Nombre,r.Autor,r.Año";
+        return "r.Id,r.Titulo,r.Autor,r.Año";
     }
     
     // Metodo para obtener el SELECT a la tabla Rol y el TOP en el caso que se utilice una base de datos SQL SERVER
@@ -46,11 +47,11 @@ public class BookDAL {
         int result;
         String sql;
         try (Connection conn = ComunDB.obtenerConexion();) { // Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
-            sql = "INSERT INTO Tareas(Name,Details,Done) VALUES(?,?,?)"; // Definir la consulta INSERT a la tabla de Rol utilizando el simbolo ? para enviar parametros
+            sql = "INSERT INTO Books(Titulo,Autor,AñoPubli) VALUES(?,?,?)"; // Definir la consulta INSERT a la tabla de Rol utilizando el simbolo ? para enviar parametros
             try (PreparedStatement ps = ComunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
                 ps.setString(1, pBook.getTitulo()); // Agregar el parametro a la consulta donde estan el simbolo ? #1  
                 ps.setString(2, pBook.getAutor()); 
-  
+                ps.setInt(2, pBook.getAñoPubli()); 
                 result = ps.executeUpdate(); // Ejecutar la consulta INSERT en la base de datos
                 ps.close(); // Cerrar el PreparedStatement
             } catch (SQLException ex) {
@@ -61,5 +62,172 @@ public class BookDAL {
             throw ex; // Enviar al siguiente metodo el error al obtener la conexion en el caso que suceda 
         }
         return result; // Retornar el numero de fila afectadas en el INSERT en la base de datos 
+    }
+    
+    // Metodo para poder actualizar un registro en la tabla de Rol
+    public static int modificar(Book pBook) throws Exception {
+        int result;
+        String sql;
+        try (Connection conn = ComunDB.obtenerConexion();) { // Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            sql = "UPDATE Books SET Tituloe=?,Autor=?,Año=? WHERE Id=?"; // Definir la consulta UPDATE a la tabla de Rol utilizando el simbolo ? para enviar parametros
+            try (PreparedStatement ps = ComunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                ps.setString(1, pBook.getTitulo()); // Agregar el parametro a la consulta donde estan el simbolo ? #1  
+                ps.setString(2, pBook.getAutor());
+                ps.setInt(3, pBook.getAñoPubli());
+                ps.setInt(4, pBook.getId()); // Agregar el parametro a la consulta donde estan el simbolo ? #2  
+                result = ps.executeUpdate(); // Ejecutar la consulta UPDATE en la base de datos
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex;  // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda 
+            }
+            conn.close(); // Cerrar la conexion a la base de datos
+        } catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion en el caso que suceda 
+        }
+        return result; // Retornar el numero de fila afectadas en el UPDATE en la base de datos 
+    }
+    
+    // Metodo para poder eliminar un registro en la tabla de Rol
+    public static int eliminar(Book pBook) throws Exception {
+        int result;
+        String sql;
+        try (Connection conn = ComunDB.obtenerConexion();) { // Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            sql = "DELETE FROM Books WHERE Id=?";  // Definir la consulta DELETE a la tabla de Rol utilizando el simbolo ? para enviar parametros
+            try (PreparedStatement ps = ComunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                ps.setInt(1, pBook.getId()); // Agregar el parametro a la consulta donde estan el simbolo ? #1 
+                result = ps.executeUpdate();  // Ejecutar la consulta DELETE en la base de datos
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex; // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda
+            }
+            conn.close();  // Cerrar la conexion a la base de datos
+        } catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion  de la clase ComunDB en el caso que suceda 
+        }
+        return result; // Retornar el numero de fila afectadas en el DELETE en la base de datos 
+    }
+    
+    // Metodo para llenar las propiedades de la entidad de Rol con los datos que viene en el ResultSet,
+    // el metodo nos ayudara a no preocuparlos por los indices al momento de obtener los valores del ResultSet
+    static int asignarDatosResultSet(Book pBook, ResultSet pResultSet, int pIndex) throws Exception {
+        //  SELECT r.Id(indice 1),r.Nombre(indice 2) * FROM Rol
+        pIndex++;
+        pBook.setId(pResultSet.getInt(pIndex)); // index 1
+        pIndex++;
+        pBook.setTitulo(pResultSet.getString(pIndex)); // index 2
+        pIndex++;
+        pBook.setAutor(pResultSet.getString(pIndex)); // index 3
+        pIndex++;
+        pBook.setAñoPubli(pResultSet.getByte(pIndex)); // index 4
+        return pIndex;
+    }
+    
+    // Metodo para  ejecutar el ResultSet de la consulta SELECT a la tabla de Rol 
+    private static void obtenerDatos(PreparedStatement pPS, ArrayList<Book> pTasks) throws Exception {
+        try (ResultSet resultSet = ComunDB.obtenerResultSet(pPS);) { // obtener el ResultSet desde la clase ComunDB
+            while (resultSet.next()) { // Recorrer cada una de la fila que regresa la consulta  SELECT de la tabla Rol
+                Book task = new Book(); 
+                asignarDatosResultSet(task, resultSet, 0); // Llenar las propiedaddes de la Entidad Rol con los datos obtenidos de la fila en el ResultSet
+                pTasks.add(task); // Agregar la entidad Rol al ArrayList de Rol
+            }
+            resultSet.close(); // Cerrar el ResultSet
+        } catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener ResultSet de la clase ComunDB   en el caso que suceda 
+        }
+    }
+    
+    // Metodo para obtener por Id un registro de la tabla de Rol 
+    public static Book obtenerPorId(Book pBook) throws Exception {
+        Book libro = new Book();
+        ArrayList<Book> tareas = new ArrayList();
+        try (Connection conn = ComunDB.obtenerConexion();) { // Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            String sql = obtenerSelect(pBook); // Obtener la consulta SELECT de la tabla Rol
+            sql += " WHERE r.Id=?"; // Concatenar a la consulta SELECT de la tabla Rol el WHERE 
+            try (PreparedStatement ps = ComunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                ps.setInt(1, pBook.getId()); // Agregar el parametro a la consulta donde estan el simbolo ? #1 
+                obtenerDatos(ps, tareas); // Llenar el ArrayList de Rol con las fila que devolvera la consulta SELECT a la tabla de Rol
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex;  // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda
+            }
+            conn.close();  // Cerrar la conexion a la base de datos
+        }
+        catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion  de la clase ComunDB en el caso que suceda
+        }
+        if (tareas.size() > 0) { // Verificar si el ArrayList de Rol trae mas de un registro en tal caso solo debe de traer uno
+            libro = tareas.get(0); // Si el ArrayList de Rol trae un registro o mas obtenemos solo el primero 
+        }
+        return libro; // Devolver el rol encontrado por Id 
+    }
+    
+    // Metodo para obtener todos los registro de la tabla de Rol
+    public static ArrayList<Book> obtenerTodos() throws Exception {
+        ArrayList<Book> libros;
+        libros = new ArrayList<>();
+        try (Connection conn = ComunDB.obtenerConexion();) {// Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            String sql = obtenerSelect(new Book());  // Obtener la consulta SELECT de la tabla Rol
+            sql += agregarOrderBy(new Book());  // Concatenar a la consulta SELECT de la tabla Rol el ORDER BY por Id 
+            try (PreparedStatement ps = ComunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                obtenerDatos(ps, libros); // Llenar el ArrayList de Rol con las fila que devolvera la consulta SELECT a la tabla de Rol
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex; // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda
+            }
+            conn.close(); // Cerrar la conexion a la base de datos
+        } 
+        catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion  de la clase ComunDB en el caso que suceda
+        }
+        return libros; // Devolver el ArrayList de Rol
+    }
+    
+    // Metodo para asignar los filtros de la consulta SELECT de la tabla de Rol de forma dinamica
+    static void querySelect(Book pBook, ComunDB.UtilQuery pUtilQuery) throws SQLException {
+        PreparedStatement statement = pUtilQuery.getStatement(); // Obtener el PreparedStatement al cual aplicar los parametros
+        if (pBook.getId() > 0) { // Verificar si se va incluir el campo Id en el filtro de la consulta SELECT de la tabla de Rol
+            pUtilQuery.AgregarWhereAnd(" r.Id=? "); // Agregar el campo Id al filtro de la consulta SELECT y agregar en el WHERE o AND
+            if (statement != null) { 
+                // Agregar el parametro del campo Id a la consulta SELECT de la tabla de Rol
+                statement.setInt(pUtilQuery.getNumWhere(), pBook.getId()); 
+            }
+        }
+        // Verificar si se va incluir el campo Nombre en el filtro de la consulta SELECT de la tabla de Rol
+        if (pBook.getTitulo()!= null && pBook.getTitulo().trim().isEmpty() == false) {
+            pUtilQuery.AgregarWhereAnd(" r.Titulo LIKE ? "); // Agregar el campo Nombre al filtro de la consulta SELECT y agregar en el WHERE o AND
+            if (statement != null) {
+                // Agregar el parametro del campo Nombre a la consulta SELECT de la tabla de Rol
+                statement.setString(pUtilQuery.getNumWhere(), "%" + pBook.getTitulo()+ "%"); 
+            }
+        }
+    }
+    
+    // Metodo para obtener todos los registro de la tabla de Rol que cumplan con los filtros agregados 
+     // a la consulta SELECT de la tabla de Rol 
+    public static ArrayList<Book> buscar(Book pBook) throws Exception {
+        ArrayList<Book> libros = new ArrayList();
+        try (Connection conn = ComunDB.obtenerConexion();) { // Obtener la conexion desde la clase ComunDB y encerrarla en try para cierre automatico
+            String sql = obtenerSelect(pBook); // Obtener la consulta SELECT de la tabla Rol
+            ComunDB comundb = new ComunDB();
+            ComunDB.UtilQuery utilQuery = comundb.new UtilQuery(sql, null, 0); 
+            querySelect(pBook, utilQuery); // Asignar el filtro a la consulta SELECT de la tabla de Rol 
+            sql = utilQuery.getSQL(); 
+            sql += agregarOrderBy(pBook); // Concatenar a la consulta SELECT de la tabla Rol el ORDER BY por Id
+            try (PreparedStatement ps = ComunDB.createPreparedStatement(conn, sql);) { // Obtener el PreparedStatement desde la clase ComunDB
+                utilQuery.setStatement(ps);
+                utilQuery.setSQL(null);
+                utilQuery.setNumWhere(0); 
+                querySelect(pBook, utilQuery);  // Asignar los parametros al PreparedStatement de la consulta SELECT de la tabla de Rol
+                obtenerDatos(ps, libros); // Llenar el ArrayList de Rol con las fila que devolvera la consulta SELECT a la tabla de Rol
+                ps.close(); // Cerrar el PreparedStatement
+            } catch (SQLException ex) {
+                throw ex;  // Enviar al siguiente metodo el error al ejecutar PreparedStatement en el caso que suceda
+            }
+            conn.close(); // Cerrar la conexion a la base de datos
+        }
+        catch (SQLException ex) {
+            throw ex; // Enviar al siguiente metodo el error al obtener la conexion  de la clase ComunDB en el caso que suceda
+        }
+        return libros; // Devolver el ArrayList de Rol
     }
 }
